d0a4d3b8fe8a412617bc19e28b5e671d1b1eab89
acemyzoe <acemyzoe@outlook.com>
2022-05-31 12:00:11 +0800

    add mqttlib

---
+++ client/libmqtt/build/CMakeFiles/mqttLib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mqttLib.dir/include/mqttLib.c.o"
+  "libmqttLib.a"
+  "libmqttLib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mqttLib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

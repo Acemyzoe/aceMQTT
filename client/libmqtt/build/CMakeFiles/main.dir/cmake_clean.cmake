d66eb90a519a2c3d7b62ee5ec6d8b7db409b2e85
acemyzoe <acemyzoe@outlook.com>
2022-05-31 10:26:50 +0800

    add lib_mqtt

---
+++ client/libmqtt/build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/mqtt_lib/lib_mqtt.c.o"
+  "CMakeFiles/main.dir/mqtt_lib_test/main.c.o"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ client/libmqtt/mqtt_lib/lib_mqtt.h
@@ -0,0 +1,53 @@
+#ifndef __LIB_MQTT_H__
+#define __LIB_MQTT_H__
+
+enum MQTT_QOS
+{
+	ONECE_MAX = 0,
+	ONECE_LATEST,
+	ONECE_ONLY
+}; //消息质量 0最多一次，1最少一次，2只有一次
+
+typedef void **MQTT_HANDLE;
+typedef void (*MSG_RECIVE_CALLBACL)(char *topic, char *msg, int msglen);
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+	/**
+	 * @brief 初始化mqtt
+	 * @param[in] clientid 客户端id
+	 * @param[in] addr 服务器地址tcp://ip:port
+	 * @param[in] username 用户名，没有验证可以为NULL
+	 * @param[in] password 密码，没有验证可以为NULL
+	 * @param[in] recive_callback 接受订阅信息回调函数
+	 * @return 		success mqtt_handle，failure NULL
+	 */
+	MQTT_HANDLE mqtt_init(char *client_id, char *addres, char *username, char *password, MSG_RECIVE_CALLBACL recive_msg_callback);
+	/**
+	 * @brief 订阅消息
+	 * @param[in] mqtt_handle mqtt句柄
+	 * @param[in] topic 订阅的主题
+	 * @param[in] qos 消息质量
+	 * @return 		success 0，failure -1
+	 */
+	int mqtt_subscribe(MQTT_HANDLE handle, char *topic, int qos);
+	/**
+	 * @brief 发布消息
+	 * @param[in] mqtt_handle mqtt句柄
+	 * @param[in] topic 订阅的主题
+	 * @param[in] msg 消息内容
+	 * @param[in] msglen 消息长度
+	 * @return 		success 0，failure -1
+	 */
+	int mqtt_publish(MQTT_HANDLE handle, char *topic, char *msg, int msg_len, int qos);
+	/**
+	 * @brief 销毁mqtt
+	 * @param[in] mqtt_handle mqtt句柄
+	 */
+	int mqtt_destroy(MQTT_HANDLE handle);
+#ifdef __cplusplus
+}
+#endif
+#endif
